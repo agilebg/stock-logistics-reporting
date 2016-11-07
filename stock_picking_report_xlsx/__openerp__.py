af558435f0ebae58fa17058eab45e2e87c3dc9c8
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2016-11-07 20:32:52 +0530

    Added XLSX report

---
+++ stock_picking_report_xlsx/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2016 Serpent Consulting Services Pvt. Ltd
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Stock picking xlsx report',
+    'version': '8.0.0.1.0',
+    'summary': "Print stock picking report in xlsx",
+    'website': 'http://www.serpentcs.com',
+    'author': """Serpent Consulting Services Pvt. Ltd.,
+                Odoo Community Association (OCA)""",
+    'category': 'Stock',
+    'license': 'AGPL-3',
+    'depends': ['report_xlsx', 'stock'],
+    'data': [
+             'views/report_view.xml'
+             ],
+    'installable': True,
+    'application': True
+}
---
+++ stock_picking_report_xlsx/report/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2016 Serpent Consulting Services Pvt. Ltd
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import stock_picking_xlsx_report 
